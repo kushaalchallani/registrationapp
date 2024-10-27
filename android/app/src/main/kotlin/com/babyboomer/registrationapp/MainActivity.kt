cbdd71fed4d77ba7df612de3735e3f1e3c6abcec
Kushaal Challani <kushaal147@gmail.com>
2024-10-27 14:17:00 +0530

    Big Updates in code

--- android/app/src/main/kotlin/com/babyboomer/registrationapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.babyboomer.registrationapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/babyboomer/simpleregistrationapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.babyboomer.simpleregistrationapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
